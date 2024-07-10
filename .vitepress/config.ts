f5edf14baf5fd46253033d991477b52299caf138
jason <1qa@WS#ed>
2024-07-10 21:27:03 +0800

    1

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -29,11 +29,33 @@ export default defineConfig({
                     {text: 'Markdown Examples', link: '/markdown-examples'},
                     {text: 'Runtime API Examples', link: '/api-examples'}
                 ]
+            },
+            {
+                text: 'interview',
+                collapsed: true,
+                items: [
+                    {text: '准备', link: '/interview/1-prepare'},
+                    {text: '程序员软件技能', link: '/interview/2-skill'},
+                    {text: 'HTML', link: '/interview/3-HTML'},
+                    {text: 'CSS', link: '/interview/4-CSS'},
+                    {text: 'JS', link: '/interview/5-JS'},
+                    {text: '性能优化', link: '/interview/6-optimize'},
+                    {text: 'Vue', link: '/interview/13-vue'},
+                    {text: 'Vue性能优化', link: '/interview/14-vueOptimize'},
+                    {text: 'React', link: '/interview/7-react'},
+                    {text: '工程化', link: '/interview/15-engineering'},
+                    {text: '网络', link: '/interview/8-network'},
+                    {text: '浏览器原理', link: '/interview/9-browser'},
+                    {text: '手写', link: '/interview/10-writeCode'},
+                    {text: '输出', link: '/interview/11-codePrint'},
+                    {text: 'LeetCode', link: '/interview/12-LeetCode'},
+                ]
             }
+
         ],
 
         socialLinks: [
-            {icon: 'github', link: 'https://github.com/vuejs/vitepress'}
+            {icon: 'github', link: 'https://github.com/xieguanhua/xieguanhua.github.io'}
         ]
     }
 })
